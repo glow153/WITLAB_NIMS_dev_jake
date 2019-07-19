7afa3f400db5a786de2c7b99bc210ea9679f60a7
glow153 <glow153@gmail.com>
2019-07-19 14:36:54 +0900

    added: cas health api

--- backend/src/api/nl/witlab/cas/cas.ctrl.js
+++ backend/src/api/nl/witlab/cas/cas.ctrl.js
@@ -1,9 +1,14 @@
-const Joi = require('joi')
-const CasSimpleEntry = require('db/models/CasSimple');
+const CasSimpleEntry = require('db/models/CasSimple')
 const Cas = require('db/models/Cas')
 const CasIrd = require('db/models/CasIrd')
-const dbg = require('lib/dbg');
+const dbg = require('lib/dbg')
+
+const Joi = require('joi')
 const moment = require('moment')
+require('moment-timezone')
+moment.tz.setDefault("Asia/Seoul")
+// const multer = require('multer')
+// const fs = require('fs')
 
 exports.root = async ctx => {
     // debug print  
@@ -45,21 +50,15 @@ exports.root = async ctx => {
         Y : entrySetY
     };
 
-    JSON.parse(JSON.stringify(entries)).forEach((element) => {
+    JSON.parse(JSON.stringify(entries)).forEach((entry) => {
 
-        entrySetX.push(moment(element.datetime));
-        // entrySetX.push(moment(element.datetime).format("HH:mm"));
-        entrySetY.push(Math.round(element.illum));
+        entrySetX.push(moment(entry.datetime));
+        // entrySetX.push(moment(entry.datetime).format("HH:mm"));
+        entrySetY.push(Math.round(entry.illum));
     })
 
     ctx.body = dataSet
 
-}
-
-exports.test = async ctx => {
-    // debug print  
-    dbg.request_log(ctx)
-    ctx.body = "ok";
 }
 
 exports.stream = async ctx => {
@@ -144,9 +143,49 @@ exports.stream_file = async ctx => {
         return
     }
 
-    const { fileobj } = ctx.request.body
+    
+    // save received file
+    fs.readFile(ctx.req.files.uploadFile.path, async (error, data) => {
+        var date = '20190717'
+        var filePath = `/home/witlab/dw/raw/${date}/${ctx.req.files.uploadFile.name}`
+        fs.writeFile(filePath, data, function(error) {
+            if (error) {
+                throw error;
+            } else {
+                //
+            }
+        })
+    })
     
 
     ctx.body = {result: 1}
 }
 
+exports.test = async ctx => {
+    // debug print  
+    dbg.request_log(ctx)
+    ctx.body = "ok";
+}
+
+exports.health = async ctx => {
+    // debug print  
+    dbg.request_log(ctx)
+    
+    const latest = await Cas.find().sort({'datetime': -1}).limit(1)  // get latest doc
+    
+    var obj_health = {
+        'status': 0,  // 0: dead, 1: alive
+        'latest_time': moment(latest[0].datetime).tz('Asia/Seoul')
+                                                 .format('YYYY-MM-DD HH:mm:ss')
+    }
+    
+    var diff = Math.abs(new Date() - latest[0].datetime)  // calc interval
+    // 만약 CAS측정이 1분 20초 동안 이루어지지 않았다면 장치측정이 멈춘 것으로 간주
+    if (diff > 80000){
+        obj_health.status = 0
+    } else {
+        obj_health.status = 1
+    }
+
+    ctx.body = obj_health
+}
--- backend/src/api/nl/witlab/cas/index.js
+++ backend/src/api/nl/witlab/cas/index.js
@@ -4,6 +4,7 @@ const cas = new Router();
 
 cas.post('/', casCtrl.root);
 cas.get('/test', casCtrl.test);
+cas.get('/health', casCtrl.health);
 cas.post('/stream', casCtrl.stream);
 cas.post('/stream_ird', casCtrl.stream_ird);
 cas.post('/stream_file', casCtrl.stream_file);
